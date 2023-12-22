60ce56759f1a2dcc6ab1c219479d8b170ed93450
Sazzad Hossen <hossain15-5144@diu.edu.bd>
2023-12-23 00:15:22 +0600

    update

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:asriftaj/google_map.dart';
+import 'package:asriftaj/latlong_test.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+void main(){
+  runApp(Myapps());
+}
+class Myapps extends StatelessWidget {
+  const Myapps({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:Googlehome(),
+    );
+  }
+}
